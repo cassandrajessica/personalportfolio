06fc849773ae7eeaaa104e88a4094ad52844d82b
cassandrajessica <clehman3@collin.edu>
2025-06-01 21:33:42 -0500

    added new elements

--- devPortfolio/src/App.jsx
+++ devPortfolio/src/App.jsx
@@ -1,12 +1,12 @@
 import { useState } from 'react'
-
 import './App.css'
+import Homepage from './components/Homepage'
 
 function App() {
 
   return (
     <>
-      
+      <Homepage />
     </>
   )
 }
